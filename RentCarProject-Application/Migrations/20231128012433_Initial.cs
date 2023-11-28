54d0d2cda5aca190cce768b3d3cc5c2e3b0a155c
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-28 23:51:40 +0500

    ozgarish kiritildi

---
+++ RentCarProject-Application/Migrations/20231128012433_Initial.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentCarProject_Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "lessor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_lessor", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "renter",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_renter", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Marka = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LessorId = table.Column<int>(type: "int", nullable: false),
+                    RenterId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cars", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_cars_lessor_LessorId",
+                        column: x => x.LessorId,
+                        principalTable: "lessor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_cars_renter_RenterId",
+                        column: x => x.RenterId,
+                        principalTable: "renter",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cars_LessorId",
+                table: "cars",
+                column: "LessorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cars_RenterId",
+                table: "cars",
+                column: "RenterId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "cars");
+
+            migrationBuilder.DropTable(
+                name: "lessor");
+
+            migrationBuilder.DropTable(
+                name: "renter");
+        }
+    }
+}
