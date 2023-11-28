6130ee45b1dcbc75496b2b3fdfae508ea3c0d38c
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-28 23:50:47 +0500

    ozgarish kiritildi

---
+++ RentCarProject.Services/Dtos/CarDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentCarProject.Services.Dtos
+{
+    public class CarDto
+    {
+        public string Name { get; set; }
+        public string Marka { get; set; }
+        public int LessorId { get; set; }
+        public int RenterId { get; set; }
+
+    }
+        
+}
