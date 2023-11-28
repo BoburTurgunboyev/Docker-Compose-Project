a5d6701655a16b6eabc71a4b861cb022111a5df0
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-29 01:43:59 +0500

    Service av Kontroller

--- RentCarProject.Services/Dtos/RenterDto.cs
+++ RentCarProject.Services/Dtos/RenterDto.cs
@@ -8,7 +8,7 @@ namespace RentCarProject.Services.Dtos
 {
     public class RenterDto
     {
-        public int Id { get; set; }
+        
         public string Name { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
