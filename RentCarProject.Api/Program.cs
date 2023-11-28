1a1d7d5062a0c8361d5c4f01c0b2a0a79eb682dd
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-28 23:50:57 +0500

    ozgarish kiritildi

--- RentCarProject.Api/Program.cs
+++ RentCarProject.Api/Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using RentCarProject.Services.Interfaces;
+using RentCarProject.Services.Services;
+using RentCarProject_Application.Data;
+using RentCarProject_Application.Interfaces;
+using RentCarProject_Application.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<RentCarDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<ICarRepository, CarRepository>();
+builder.Services.AddScoped<ICarService,CarService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
