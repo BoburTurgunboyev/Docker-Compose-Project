0ec73d164910cf4c2c78a269326e94946d5fa0a4
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-29 01:43:54 +0500

    Service av Kontroller

--- RentCarProject.Services/Interfaces/IBaseService.cs
+++ RentCarProject.Services/Interfaces/IBaseService.cs
@@ -4,10 +4,12 @@ public interface IBaseService<Dto, Entity>
 {
     public ValueTask<bool> CreateAsync(Dto entity);
 
-    public ValueTask<bool> UpdateAsync(long id, Dto dto);
+    public ValueTask<bool> UpdateAsync(int id, Dto dto);
 
     public ValueTask<bool> DeleteAsync(int id);
 
     public ValueTask<IList<Entity>> GetAllAsync();
+
+    public ValueTask<Entity> GetByIdAsync(int id);
 
 }
