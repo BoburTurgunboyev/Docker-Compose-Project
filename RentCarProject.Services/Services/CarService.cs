3fb73bdad2c09ca5b142eaf2eba8ff456156a1b1
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-28 23:50:07 +0500

    ozgarish kiritildi

---
+++ RentCarProject.Services/Services/CarService.cs
@@ -0,0 +1,48 @@
+﻿using RentCarProject.Domain.Entities.Models;
+using RentCarProject.Services.Dtos;
+using RentCarProject.Services.Interfaces;
+using RentCarProject_Application.Interfaces;
+using RentCarProject_Application.Repositories;
+
+namespace RentCarProject.Services.Services
+{
+    public class CarService : ICarService
+    {
+        private readonly ICarRepository _carRepository;
+
+        public CarService(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+        public async ValueTask<bool> CreateAsync(CarDto entity)
+        {
+            Car car = new Car() 
+            {
+                Name = entity.Name,
+                Marka = entity.Marka,
+                LessorId = entity.LessorId,
+                RenterId = entity.RenterId,
+                
+
+            };
+            var res =await _carRepository.CreateAsync(car);
+            return res;
+        }
+
+        public async ValueTask<bool> DeleteAsync(int id)
+        {
+            var caar = await _carRepository.DeleteAsync(id);    
+            return caar;
+        }
+
+        public async ValueTask<IList<Car>> GetAllAsync()
+        {
+            return await _carRepository.GetAllAsync();    
+        }
+
+        public ValueTask<bool> UpdateAsync(long id, CarDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
