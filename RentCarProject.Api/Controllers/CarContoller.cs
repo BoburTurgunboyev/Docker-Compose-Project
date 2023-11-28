e3ccefcb9ef9bdb6ae6a9532455834314c733ed0
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-29 01:44:19 +0500

    Service av Kontroller

--- RentCarProject.Api/Controllers/CarContoller.cs
+++ RentCarProject.Api/Controllers/CarContoller.cs
@@ -25,5 +25,26 @@ namespace RentCarProject.Api.Controllers
         {
             return Ok(await _carService.GetAllAsync());
         }
+
+        [HttpDelete]
+
+        public async ValueTask<IActionResult> DeleteAsync(int id)
+        {
+            return Ok(await _carService.DeleteAsync(id));
+        }
+
+        [HttpPut]
+
+        public async ValueTask<IActionResult> UpdateAsync( int id,CarDto carDto) 
+        {
+            return Ok(await _carService.UpdateAsync(id,carDto));
+        }
+
+        [HttpGet]
+
+        public async ValueTask<IActionResult> GetByIdAsync(int id)
+        {
+            return Ok(await _carService.GetByIdAsync(id));
+        }
     }
 }
