8616ec54613e2a8c00355de60638922819b1d713
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-28 23:50:24 +0500

    ozgarish kiritildi

---
+++ RentCarProject.Services/Interfaces/ICarService.cs
@@ -0,0 +1,9 @@
+﻿using RentCarProject.Domain.Entities.Models;
+using RentCarProject.Services.Dtos;
+
+namespace RentCarProject.Services.Interfaces;
+
+public interface ICarService : IBaseService<CarDto, Car>
+{
+
+}
