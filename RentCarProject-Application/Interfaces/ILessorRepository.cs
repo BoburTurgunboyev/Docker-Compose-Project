4b4d6699f3a8921eb4182bff942bf75d2cf6618f
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-28 23:51:50 +0500

    ozgarish kiritildi

---
+++ RentCarProject-Application/Interfaces/ILessorRepository.cs
@@ -0,0 +1,13 @@
+﻿using RentCarProject.Domain.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentCarProject_Application.Interfaces
+{
+    public interface ILessorRepository : IBaseRepository<Lessor>
+    {
+    }
+}
