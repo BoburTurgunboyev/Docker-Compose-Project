d73fd095f9fef2b3338cb976f6bc7438db8ef484
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-28 23:50:02 +0500

    ozgarish kiritildi

---
+++ RentCarProject.Services/Services/LessorService.cs
@@ -0,0 +1,29 @@
+﻿using RentCarProject.Domain.Entities.Models;
+using RentCarProject.Services.Dtos;
+using RentCarProject.Services.Interfaces;
+
+namespace RentCarProject.Services.Services
+{
+    public class LessorService : ILessorService
+    {
+        public ValueTask<bool> CreateAsync(LessorDto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<IList<Lessor>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<bool> UpdateAsync(long id, LessorDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
