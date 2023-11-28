66f5f60aecaff423d7ecae9866d26aaaf8afaeac
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-28 23:50:29 +0500

    ozgarish kiritildi

---
+++ RentCarProject.Services/Interfaces/IBaseService.cs
@@ -0,0 +1,13 @@
+﻿namespace RentCarProject.Services.Interfaces;
+
+public interface IBaseService<Dto, Entity>
+{
+    public ValueTask<bool> CreateAsync(Dto entity);
+
+    public ValueTask<bool> UpdateAsync(long id, Dto dto);
+
+    public ValueTask<bool> DeleteAsync(int id);
+
+    public ValueTask<IList<Entity>> GetAllAsync();
+
+}
