aaacb401faecd123538cdc51abdfefe1ef352441
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-29 01:43:48 +0500

    Service av Kontroller

--- RentCarProject.Services/Services/CarService.cs
+++ RentCarProject.Services/Services/CarService.cs
@@ -2,47 +2,61 @@
 using RentCarProject.Services.Dtos;
 using RentCarProject.Services.Interfaces;
 using RentCarProject_Application.Interfaces;
-using RentCarProject_Application.Repositories;
 
-namespace RentCarProject.Services.Services
+namespace RentCarProject.Services.Services;
+
+public class CarService : ICarService
 {
-    public class CarService : ICarService
+    private readonly ICarRepository _carRepository;
+
+    public CarService(ICarRepository carRepository)
     {
-        private readonly ICarRepository _carRepository;
-
-        public CarService(ICarRepository carRepository)
-        {
-            _carRepository = carRepository;
-        }
-        public async ValueTask<bool> CreateAsync(CarDto entity)
-        {
-            Car car = new Car() 
-            {
-                Name = entity.Name,
-                Marka = entity.Marka,
-                LessorId = entity.LessorId,
-                RenterId = entity.RenterId,
-                
-
-            };
-            var res =await _carRepository.CreateAsync(car);
-            return res;
-        }
-
-        public async ValueTask<bool> DeleteAsync(int id)
-        {
-            var caar = await _carRepository.DeleteAsync(id);    
-            return caar;
-        }
-
-        public async ValueTask<IList<Car>> GetAllAsync()
-        {
-            return await _carRepository.GetAllAsync();    
-        }
-
-        public ValueTask<bool> UpdateAsync(long id, CarDto dto)
-        {
-            throw new NotImplementedException();
-        }
+        _carRepository = carRepository;
     }
+    public async ValueTask<bool> CreateAsync(CarDto entity)
+    {
+        Car car = new Car()
+        {
+            
+            Name = entity.Name,
+            Marka = entity.Marka,
+            LessorId = entity.LessorId,
+            RenterId = entity.RenterId,
+
+
+        };
+        var res = await _carRepository.CreateAsync(car);
+        return res;
+    }
+
+    public async ValueTask<bool> DeleteAsync(int id)
+    {
+        var caar = await _carRepository.DeleteAsync(id);
+        return caar;
+    }
+
+    public async ValueTask<IList<Car>> GetAllAsync()
+    {
+        return await _carRepository.GetAllAsync();
+    }
+
+    public async ValueTask<Car> GetByIdAsync(int id)
+    {
+        var result = await _carRepository.GetByIdAsync(id);
+        return result;
+    }
+
+    public async ValueTask<bool> UpdateAsync(int id, CarDto dto)
+    {
+        var res = await _carRepository.GetByIdAsync(id); 
+        res.Name = dto.Name;
+        res.Marka = dto.Marka;
+
+        var res1 = await _carRepository.UpdateAsync(id, res);
+
+        return res1;
+
+    }
+
+
 }
