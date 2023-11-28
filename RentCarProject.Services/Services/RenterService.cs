a7783bd1209010283efbf1c37279924db06cd63a
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-28 23:49:56 +0500

    ozgarish kiritildi

---
+++ RentCarProject.Services/Services/RenterService.cs
@@ -0,0 +1,29 @@
+﻿using RentCarProject.Domain.Entities.Models;
+using RentCarProject.Services.Dtos;
+using RentCarProject.Services.Interfaces;
+
+namespace RentCarProject.Services.Services
+{
+    public class RenterService : IRenterService
+    {
+        public ValueTask<bool> CreateAsync(RenterDto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<IList<Renter>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<bool> UpdateAsync(long id, RenterDto dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
