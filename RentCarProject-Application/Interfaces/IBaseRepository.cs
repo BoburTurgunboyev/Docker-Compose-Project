701e61010fd173742e9336f2477147e7a6b9a230
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-28 23:52:00 +0500

    ozgarish kiritildi

---
+++ RentCarProject-Application/Interfaces/IBaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentCarProject_Application.Interfaces
+{
+    public interface IBaseRepository<Entity> where Entity : class
+    {
+        public ValueTask<bool> CreateAsync(Entity entity);
+
+        public ValueTask<bool> UpdateAsync(long id, Entity entity);
+
+        public ValueTask<bool> DeleteAsync(int id);
+
+        public ValueTask<IList<Entity>> GetAllAsync();
+
+       
+    }
+}
