1119a70175d5a6508630623c67a3fff22ba520b4
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-28 23:49:41 +0500

    ozgarish kiritildi

--- RentCarProject-Application/Data/RentCarDBContext.cs
+++ RentCarProject-Application/Data/RentCarDBContext.cs
@@ -1,7 +1,17 @@
-﻿namespace RentCarProject_Application.Data
-{
-    public class RentCarDBContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using RentCarProject.Domain.Entities.Models;
 
+namespace RentCarProject_Application.Data
+{
+    public class RentCarDBContext : DbContext
+    {
+        public RentCarDBContext(DbContextOptions<RentCarDBContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Car> cars { get; set; }
+        public DbSet<Lessor> lessor { get; set; }   
+        public DbSet<Renter> renter { get; set; }   
     }
 }
