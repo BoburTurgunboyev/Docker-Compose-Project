5617198ed0aadfec01639f5b537008970f3a7174
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-28 23:51:07 +0500

    ozgarish kiritildi

---
+++ RentCarProject.Api/Controllers/CarContoller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentCarProject.Services.Dtos;
+using RentCarProject.Services.Interfaces;
+using RentCarProject.Services.Services;
+
+namespace RentCarProject.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CarContoller : ControllerBase
+    {
+        private readonly ICarService _carService;
+
+        public CarContoller(ICarService carService)
+        {
+            _carService = carService;
+        }
+        [HttpPost]
+        public async ValueTask<ActionResult> CreateAsync( CarDto carDto)
+            => Ok(await _carService.CreateAsync(carDto));
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            return Ok(await _carService.GetAllAsync());
+        }
+    }
+}
