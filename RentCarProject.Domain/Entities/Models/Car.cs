6ce552ca9a263c124066e8f4be85defffa1b9d88
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2023-11-29 18:58:22 +0500

    Docker.yml lar qoshdim

--- RentCarProject.Domain/Entities/Models/Car.cs
+++ RentCarProject.Domain/Entities/Models/Car.cs
@@ -1,17 +1,15 @@
 ﻿using System.Diagnostics.Metrics;
 
-namespace RentCarProject.Domain.Entities.Models
+namespace RentCarProject.Domain.Entities.Models; 
+
+
+public class Car
 {
-    public class Car
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Marka { get; set; }
-
-        public int LessorId { get; set; }
-        public Lessor Lessor { get; set; }
-
-        public int RenterId { get; set; }
-        public Renter Renter { get; set; }
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Marka { get; set; }
+    public int LessorId { get; set; }
+    public Lessor Lessor { get; set; }
+    public int RenterId { get; set; }
+    public Renter Renter { get; set; }
 }
